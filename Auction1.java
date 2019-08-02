1ce31754a1d13d2b46574f4d0030cea1bd0e2645
Roop98 <roopranjank@gmail.com>
2019-08-02 23:19:05 +0300

    oh yeah

---
+++ Auction1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Auction1 {
+
+    public static void main(String[] args) {
+
+        Auction auction = new Auction();
+        auction.list();
+        auction.randomauction();
+    }
+}
