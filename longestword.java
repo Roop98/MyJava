dda564391aa3c3d8ffcc96885e22afdc8a0e1b58
Roop98 <roopranjank@gmail.com>
2019-08-02 22:43:48 +0300

    my new java file

---
+++ longestword.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class longestword {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String word = scan.next();
+        char[] wo = word.toCharArray();
+        for (int i =0;i<wo.length;i++)
+        {
+
+        }
+    }
+}
